297c6cfe9509d336aeea8f338183e6b167f95cb0
phelipe <viana@dadosmobile.com>
2022-03-19 19:00:36 -0400

    Jwt no Controller

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -1,34 +1,28 @@
 const conexao = require("./conexao");
 const md5 = require('md5');
-const responseModel = {
-    success: false,
-    msg: '',
-    data: [],
-    error: [],
-    token: ''
-}
+const jwt = require('jsonwebtoken');
+const SECRET = 'phelipe';
+
+
 module.exports = {
     async Login(req, res) {
+
         let SQL = 'SELECT * FROM login where email=? and senha=?';
         let VAL = [req.body.email, md5(req.body.senha)];
-        let response = { ...responseModel }
+      
 
         conexao.promise().query(SQL, VAL)
             .then(([rows]) => {
                 if (rows.length > 0) {
-                    response.success = true;
-                    response.msg = 'Logado com sucesso';
-                    return res.json(response)
+                    const token = jwt.sign({ userId: 1 }, SECRET, { expiresIn: 300 })
+                    return res.status(200).json({ auth: true, token })
                 } else {
                     response.msg = 'Verficar credenciais';
-                    return res.json(response)
+                    return res.json({ auth: false})
                 }
             })
             .catch((err) => {
-                response.success = false;
-                response.error = err;     
-                
-                return res.json(response)
+                return res.status(401).end()
             })
     }
 }
--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 const express = require('express')
 const app = express();
 const porta = 3000;
-const session = require('express-session');
-const Controller=require('./Controller/Controller')
+const Controller = require('./Controller/Controller')
+const jwt = require('jsonwebtoken');
+const SECRET = 'phelipe';
 
-app.use(session({
-    secret: 'phelipe',
-    resave: false,
-    saveUninitialized: false,
-}))
+function VerifyJWT(req, res, next) {
+    const token = req.headers['x-access-token'];
+    jwt.verify(token, SECRET, (error, decoded) => {
+        if (error) return res.status(401).end();
+        req.userId = decoded.userId
+        next();
+    });
+
+
+}
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended:false}))
-//app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-app.post('/login',Controller.Login)
-
+app.post('/login', Controller.Login)
+app.get('/logout', function (req, res) {
+    blacklist.push(req.headers['x-access-token'])
+    res.end()
+})
 app.get('/', function (req, res) {
     res.render('../views/index');
-   
+
 })
 
-app.get('/teste', function (req, res) {
-    if (!req.session.login) {
-        res.redirect('/');
-        console.log('tentou entrar na pagina restrita')
-    } else {
-        res.render('../views/teste')
-    }
-
+app.get('/teste', VerifyJWT, function (req, res) {
+    //console.log(VerifyJWT)
+    res.render('../views/teste')
 })
 
 app.listen(porta, () => { console.log(`porta ${porta}`) })
